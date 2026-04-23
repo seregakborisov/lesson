b87f13166bb609fe964143e7f76c0688d1561bff
seregakborisov <kurak93.sk@gmail.com>
2026-04-23 21:34:51 +0300

    Работа с ОС

---
+++ lesson7/ex1.py
@@ -0,0 +1,11 @@
+import platform
+import os
+
+print(f"Версия операционной: {platform.platform()}, {platform.system()}")
+
+print("-"*20)
+
+print(f"Путь к файлу: {os.getcwd()}") 
+
+print("-"*20)
+
