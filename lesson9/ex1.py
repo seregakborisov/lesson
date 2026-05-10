4546abedaa035aa53fc0b92741314dff6ed2765a
seregakborisov <kurak93.sk@gmail.com>
2026-05-10 23:13:56 +0300

    lesson9

---
+++ lesson9/ex1.py
@@ -0,0 +1,43 @@
+class product:
+    def __init__(self, product_name, store_name, cost):
+        self.product_name = product_name
+        self.store_name = store_name
+        self.cost = cost
+    def show(self):
+        print(self.product_name, self.store_name, self.cost)
+    
+class storage:
+    def __init__(self):
+        self.__products = []
+       
+    def product_index(self, index:int):
+        return self.__products[index]
+        
+    def product_for_name(self):
+        pass
+    
+    def sort_by_name(self):
+        pass
+    
+    def sort_by_store(self):
+        pass
+    
+    def sort_by_cost(self):
+        pass
+    
+    def sum_product(self):
+        pass
+
+
+
+product1 = product("Сыр", "Евроопт", 12)
+product2 = product("Колбоса", "Евроопт", 9)
+product3 = product("Картофель", "Корона", 1.2)
+product4 = product("Томат", "Санты", 18.5)
+product5 = product("Яйца", "Алми", 4)
+x = 5
+storage1 = storage()
+
+z = product.show
+print(product1)
+print(z)
