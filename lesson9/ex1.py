e3fe6f83b42086b01c783febefc68e0fd2f1cfa8
seregakborisov <kurak93.sk@gmail.com>
2026-05-11 22:56:36 +0300

    lesson9

--- lesson9/ex1.py
+++ lesson9/ex1.py
@@ -3,15 +3,20 @@ class product:
         self.product_name = product_name
         self.store_name = store_name
         self.cost = cost
-    def show(self):
-        print(self.product_name, self.store_name, self.cost)
+        print("Товар: " + product_name + ". Магазин: " + store_name + ". Стоимость:", cost, "руб.") 
+    
+    def __add__(self, other):
+        return self.cost + other.cost 
+     
+        
     
 class storage:
     def __init__(self):
-        self.__products = []
+        self.products = []
        
     def product_index(self, index:int):
-        return self.__products[index]
+        if index >= 0:
+            self.products[index].show()
         
     def product_for_name(self):
         pass
@@ -35,9 +40,7 @@ product2 = product("Колбоса", "Евроопт", 9)
 product3 = product("Картофель", "Корона", 1.2)
 product4 = product("Томат", "Санты", 18.5)
 product5 = product("Яйца", "Алми", 4)
-x = 5
+x = product1 + product2
 storage1 = storage()
 
-z = product.show
-print(product1)
-print(z)
+print(x)
